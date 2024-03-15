cc570115926757346d4e45da7e321d994d8612d4
zerais <davesiarez18@gmail.com>
2024-03-15 08:32:30 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-[#302F2A] pt-20">
+      <View className="flex justify-center items-center w-[100%]">
+        <Text className="text-white text-2xl">
+          <Text className="text-3xl">Chess</Text>.com
+        </Text>
+      </View>
+
+      <View className="flex-1 justify-start items-center bg-[#272521] m-4 rounded-2xl h-[80%]">
+        <View className="flex h-[40%] w-[80%] justify-center items-center border">
+          <TextInput placeholder="Username or Email" placeholderTextColor="#6F6D6D" className="w-[100%] border border-white text-white p-2"></TextInput>
+          <TextInput placeholder="Password" placeholderTextColor="#6F6D6D" className="w-[100%] border border-white text-white m-2 rounded-sm"></TextInput>
+
+        </View>
+
+      </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
